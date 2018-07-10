b736e8abf584d48bbaa07b1a463e140dd9b4c3e0
Christofer Pak <christoferpak@gmail.com>
2018-07-10 23:26:33 +0200

    oauth: "login" flow working

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ require('dotenv').config()
 
 const authRequest = require('./lib/auth/request')
 const authorize = require('./lib/auth/authorize')
+const accessToken = require('./lib/auth/exchange')
 
-const main = authRequest
+const main = (key, secret) => authRequest(key, secret)
+  .then(({ token, tokenSecret }) => authorize(token)
+    .then(verifier => accessToken(key, secret)(token, tokenSecret, verifier)))
 
 main(process.env.KEY, process.env.SECRET)
-  .then(authorize)
-  .then(console.log)
+  .then(console.log, console.error)
